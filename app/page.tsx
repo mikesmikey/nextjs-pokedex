7914922b356a1ecccb95f4e2fc3eb6025e02e2d1
Kraiwich <le_kraiwich@hotmail.com>
2023-10-05 04:53:07 +0700

    add missing files to the last commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,11 @@ import LogoImg from '@/assets/logo.png'
 export default async function Home() {
   const pokemons = await getPokemons()
   return (
-    <main className="h-screen flex flex-col items-center justify-center gap-6">
-      <Image src={LogoImg} alt="logo"/>
+    <main className="h-screen flex flex-col items-center justify-center gap-6 text-white">
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className="text-9xl font-thin">Pokédex</h1>
+        <h4 className="text-2xl font-light">A Database for 1st Generation Pokemons</h4>
+      </div>
       <Input pokemons={pokemons} />
     </main>
   )
