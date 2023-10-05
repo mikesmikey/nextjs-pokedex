21b675a73035f258c64257b910283b29aa37cde2
Kraiwich <le_kraiwich@hotmail.com>
2023-10-05 07:47:29 +0700

    fix responsive view margin bottom in navigation bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,13 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-import LogoImg from '@/assets/logo.png'
 import Input from './Input'
 import { getPokemons } from '@/apis/pokemon.api'
 
 export default async function Navbar() {
     const pokemons = await getPokemons()
     return (
-        <nav className='w-full flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between items-center p-4 backdrop-blur-lg bg-white/30 shadow-md mb-12 sticky top-0 z-50'>
+        <nav className='w-full flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between items-center p-4 backdrop-blur-lg bg-white/30 shadow-md mb-0 md:mb-12 sticky top-0 z-50'>
             <Link href={`/`}>
                 <h1 className='text-3xl font-thin text-white'>Pokédex</h1>
             </Link>
